4329d0214a20527eba88a22e8c3f7d696eaf0124
Aladin <aladin8857@gmail.com>
2020-08-29 11:14:11 +0100

    exo1

---
+++ TP-Tableaux/exo1/remplissage.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 50
+#define MIN -50
+
+
+int main(int argc, char const *argv[])
+{
+    int tableau[10];
+    srand(time(NULL));
+
+    for (int i = 0; i < 10; i++)
+    {
+        tableau[i] = rand() % (MAX - MIN - 1) + 1;
+    }
+    
+    printf("---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+\n");
+    printf("|");
+    for (int u = 0; u < 10; u++)
+    {
+        printf("%7d |",tableau[u]);
+    }
+    printf("\n---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+\n");
+
+    
+
+    return 0;
+}
