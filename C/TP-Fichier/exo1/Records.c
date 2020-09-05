a5ad97921091fc733ba42f85f9e3c895bcaed55f
Aladin <aladin8857@gmail.com>
2020-09-05 14:05:15 +0200

    exo1

---
+++ C/TP-Fichier/exo1/Records.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int i,score = 0;
+    char buffer[3];
+
+    FILE *fichier = NULL;
+    fichier = fopen("top10","r+");
+
+    if (fichier == NULL)
+    {
+        printf("Error : Impossible d'ouvrir le fichier !");
+        exit(0);
+    }
+    else
+    {
+        printf("Success : Ouverture du fichier reussi !\n");
+
+        for (i = 0; i <= 10; i++)
+        {
+           fread(&score,sizeof(int),1,fichier);
+           fread(&buffer,sizeof(char),3,fichier);
+           printf("\n%d %s \n",score,buffer);
+           score = 0;
+        }
+        
+
+
+        fclose(fichier);
+    }
+    
+    
+
+
+
+    return 0;
+}
