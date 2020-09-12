fb0f4ca55d41a6356b48ca33252fd8df7f899ca5
Aladin <aladin8857@gmail.com>
2020-09-12 18:25:49 +0200

    open,write,read

---
+++ C/Autres/Write,read&open/Fichier_avec_write_read.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char const *argv[])
+{
+    
+	int lecture,ecriture;
+	int rd,wrt;
+	int taille_texte = 0;
+	char buf[10];
+	pid_t fils;
+
+	lecture = open("texte.txt",O_RDONLY);
+	ecriture = open("texte_vide.txt",O_RDWR);
+
+		if (lecture == -1 || ecriture == -1)
+		{
+			printf("Erreur :L'ouverture a échoué ! \n");
+			exit(1);
+		}
+		else
+		{
+			printf("Succes : Ouverture réussi !\n");
+			
+
+				while(1)
+				{
+					rd = read(lecture,&buf,1);
+					taille_texte++;
+					printf("%d\n",taille_texte );
+					if (rd <= 0)
+					{
+						break;
+					}
+				}
+				close(lecture);
+
+
+				lecture = open("texte.txt",O_RDONLY);
+
+				for (int i = 0; i < taille_texte; ++i)
+				{
+					rd = read(lecture,&buf,1);
+					wrt = write(ecriture,&buf,1);
+				}
+
+		}
+
+    
+    return 0;
+}
